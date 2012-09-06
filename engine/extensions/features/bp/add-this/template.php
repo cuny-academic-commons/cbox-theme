eb7cf2009515db61b2ff94e2207ee0f43bdad9b2
Bowe Frankema <bowe@presscrew.com>
2012-09-06 13:20:41 +0200

    Update social sharing code

--- engine/extensions/features/bp/add-this/template.php
+++ engine/extensions/features/bp/add-this/template.php
@@ -13,12 +13,13 @@
 
 /* @var $this ICE_Feature_Renderer */
 ?>
-<div <?php $this->render_attrs() ?>>
-	<a class="addthis_button_preferred_1"></a>
-	<a class="addthis_button_preferred_2"></a>
-	<a class="addthis_button_preferred_3"></a>
-	<a class="addthis_button_preferred_4"></a>
-	<a class="addthis_button_compact"></a>
-	<a class="addthis_counter addthis_bubble_style"></a>
-	<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js"></script>
+<!-- AddThis Button BEGIN -->
+<div class="addthis_toolbox addthis_default_style ">
+<a class="addthis_button_preferred_1"></a>
+<a class="addthis_button_preferred_2"></a>
+<a class="addthis_button_preferred_3"></a>
+<a class="addthis_button_preferred_4"></a>
+<a class="addthis_button_compact"></a>
+<a class="addthis_counter addthis_bubble_style"></a>
 </div>
+<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=xa-50477e023680eef3"></script>
