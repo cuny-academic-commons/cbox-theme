63110e1ea8dec7efaf91d6d179eb37356162fec4
Bowe Frankema <bowe@presscrew.com>
2012-10-22 16:33:27 +0200

    Update Theme Dashboard for CBOX v1

--- dashboard/templates/screens/dashboard-sidebar.php
+++ dashboard/templates/screens/dashboard-sidebar.php
@@ -3,61 +3,37 @@
 	<div id="infinity-sidebar">
 	
 	<div class="dashboard-widget">
+		<h3>Online Documentation</h3>
+	
+		<p>
+Learn more how to use Commons in a Box as a site owner or developer by visiting our online documentation. It will guide you through setting up Commons in a Box and getting the most out of it's many features.
+<br>
+<br>
+<a class="button" href="http://commonsinabox.org/documentation" title="Commons in a Box Documentation">Go to the Documentation</a>
+		</p>
+	
+	</div>
+
+	
+	<div class="dashboard-widget">
 		
-	<h3>Follow PressCrew</h3>
+	<h3>Follow us on Twitter</h3>
 	
 	<p>
-		Stay up to date about Infinity and our new themes and products by following us on Twitter.</br>
+		Stay up to date about the Commons in a Box project bij following us on Twitter.<br>
+<br>
 		<a href="https://twitter.com/PressCrew" class="twitter-follow-button">Follow @PressCrew</a>
 		<script src="//platform.twitter.com/widgets.js" type="text/javascript"></script>
 	</p>
 		
 	</div>
-	
-	<div class="dashboard-widget">
-	<h3>Subscribe to our newsletter</h3>
-	We'll send you the occasional newsletter telling your about our new themes and plugins. No spam. Promised!
-	<p>
-		<form action="http://cfcommunity.us1.list-manage1.com/subscribe/post?u=34231a6f5a96be54f1f5bfc09&amp;id=98a5ecaa92" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
-			<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Your email...." required>
-			<br>
-		<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
-		<br>
-	</form>
-	</p>
-	</div>
-	
+		
 	<div class="dashboard-widget">
 		<?php //display some useful support info
 			infinity_support_info();
 		?>
 	</div>	
-	
-		<div class="dashboard-widget">
-			<h3>
-				Learn Infinity
-			</h3>
 		
-			<p>
-				The theme developer documentation is available right in the theme, as well as online!
-				Its the best place to start.
-				
-				<ul class="theme-features">
-					<li id="responsive">
-						<a target="infinity-cpanel-tab-ddocs" href="<?php print infinity_screens_route( 'cpanel', 'ddocs', 'index' ) ?>">Built-In Docs &raquo;</a>
-					</li>
-					<li id="slider">
-						<a target="_blank" href="http://infinity.presscrew.com/docs">Online Docs &raquo;</a>
-					</li>
-					<li id="easy-hooks">
-						<a target="_blank" href="http://infinity.presscrew.com/api">Online API Manual &raquo;</a>
-					</li>
-				</ul>
-			</p>
-		
-		</div>
-	
-	
 </div>
 <!-- Three Widgets end -->
 
