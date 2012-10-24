4f6bb86e8c6d41a08b375135e3cab810ffdfb337
Marshall Sorenson <marshall@presscrew.com>
2012-10-24 13:40:53 -0400

    cleaned up functions.php file

--- functions.php
+++ functions.php
@@ -1,41 +1,8 @@
 <?php
-//Custom functionality for Cbox
+
+/**
+ * Custom functionality for Cbox
+ */
 require_once( 'engine/includes/cbox-base.php' );
 
-/**
- * Set this to true to put Infinity into developer mode
- */
-//define( 'INFINITY_DEV_MODE', true );
-
-//
-// Usually dev mode is enough, but if want finer control you can
-// set some of these special constants manually.
-//
-
-/**
- * Set this to false to totally disable error handling by Infinity
- */
-//define( 'INFINITY_ERROR_HANDLING', false );
-
-/**
- * Set this to true to show detailed error and exception reports. This only
- * works if error handling is enabled (see above)
- */
-//define( 'INFINITY_ERROR_REPORTING', true );
-
-/**
- * Set this to a short message to customize the friendly AJAX error
- */
-//define( 'ICE_ERROR_AJAX_MESSAGE', 'Woops! We forgot to feed the server, sorry!' );
-
-/**
- * Set this to an absolute path to a custom friendly fatal error page
- */
-//define( 'ICE_ERROR_PAGE_PATH', '/path/to/my/error.php' );
-
-/**
- * Set this to false to disable caching of dynamically generated
- * CSS and Javascript. Highly recommended for development.
- */
-//define( 'ICE_CACHE_EXPORTS', false );
 ?>
