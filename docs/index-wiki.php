4f97876254ac28afa8cedcc39fcd388919045589
r-a-y <r-a-y@gmx.com>
2013-05-23 13:36:43 -0700

    Add customized BP Docs Wiki template.
    The newer version of BP Docs Wiki uses theme compat, however we want to be
    able to use the older template format to use the wiki sidebar.
    
    For more details, see:
    https://github.com/boonebgorges/buddypress-docs-wiki/issues/8

---
+++ docs/index-wiki.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Customized BP Docs Wiki homepage template.
+ *
+ * Basically copies page.php and adds some code to display the wiki widget
+ * sidebars.
+ */
+
+infinity_get_header();
+?>
+	<div id="content" role="main" class="<?php do_action( 'content_class' ); ?>">
+		<?php
+			do_action( 'open_content' );
+			do_action( 'open_page' );
+
+			infinity_get_template_part( 'templates/loops/loop', 'page' );
+		?>
+
+		<div class="wiki-home">
+			<div class="wiki-home-sidebar" id="wiki-top">
+				<?php dynamic_sidebar( 'wiki-top' ) ?>
+			</div>
+
+			<div id="wiki-bottom">
+				<div class="wiki-home-sidebar" id="wiki-bottom-left">
+					<?php dynamic_sidebar( 'wiki-bottom-left' ) ?>
+				</div>
+
+				<div class="wiki-home-sidebar" id="wiki-bottom-right">
+					<?php dynamic_sidebar( 'wiki-bottom-right' ) ?>
+				</div>
+			</div>
+
+
+			<?php bp_get_template_part( 'docs/docs-loop' ); ?>
+		</div><!-- .wiki-home -->
+
+		<?php
+			do_action( 'close_page' );
+			do_action( 'close_content' );
+		?>
+	</div>
+
+<?php
+// begin copy of sidebar.php
+// need to copy infinity's sidebar markup
+do_action( 'before_sidebar' );
+?>
+
+<aside id="sidebar" role="complementary" class="<?php do_action( 'sidebar_class' ); ?>">
+	<?php
+		do_action( 'open_sidebar' );
+		dynamic_sidebar( 'wiki-sidebar' );
+		do_action( 'close_sidebar' );
+		do_action( 'after_sidebar' );
+	?>
+</aside>
+
+<?php
+// end copy of sidebar.php
+
+// load infinity's footer
+infinity_get_footer();
