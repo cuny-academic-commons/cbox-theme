4bca10469589446288df926610d605b447d5acf0
Boone B Gorges <boonebgorges@gmail.com>
2016-12-06 10:53:20 -0600

    Improve indentation in comments.php to clarify if/then logic.
    See #247.

--- comments.php
+++ comments.php
@@ -32,44 +32,44 @@ if ( post_password_required() ) { ?><p class="nocomments"><?php _e('This post is
  if ( have_comments() ) {
  
  	if ( ! empty($comments_by_type['comment']) ) { ?>
- 	<h3 id="comments-title"><?php printf( _n( 'One Response to %2$s', '%1$s Responses to %2$s', get_comments_number(), infinity_text_domain ), number_format_i18n( get_comments_number() ), '<em>' . get_the_title() . '</em>' ); ?></h3>
- 	<ol class="commentlist">
+		<h3 id="comments-title"><?php printf( _n( 'One Response to %2$s', '%1$s Responses to %2$s', get_comments_number(), infinity_text_domain ), number_format_i18n( get_comments_number() ), '<em>' . get_the_title() . '</em>' ); ?></h3>
+		<ol class="commentlist">
+			<?php
+				/* Loop through and list the comments. Tell wp_list_comments()
+				 * to use custom_comment() to format the comments.
+				 * If you want to overload this in a child theme then you can
+				 * define custom_comment() and that will be used instead.
+				 * See custom_comment() in /includes/theme-comments.php for more.
+				 */
+				wp_list_comments( array( 'callback' => 'custom_comment', 'type' => 'comment', 'avatar_size' => 40 ) );
+			?>
+		</ol>
 		<?php
-			/* Loop through and list the comments. Tell wp_list_comments()
-			 * to use custom_comment() to format the comments.
-			 * If you want to overload this in a child theme then you can
-			 * define custom_comment() and that will be used instead.
-			 * See custom_comment() in /includes/theme-comments.php for more.
-			 */
-			wp_list_comments( array( 'callback' => 'custom_comment', 'type' => 'comment', 'avatar_size' => 40 ) );
-		?>
-	</ol>
- 	<?php
- 	// Comment pagination.
- 	if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) { ?>
-	<div class="navigation">
-		<div class="nav-previous fl"><?php previous_comments_link( __( '<span class="meta-nav">&larr;</span> Older Comments', infinity_text_domain ) ); ?></div>
-		<div class="nav-next fr"><?php next_comments_link( __( 'Newer Comments <span class="meta-nav">&rarr;</span>', infinity_text_domain ) ); ?></div>
-		<div class="fix"></div><!--/.fix-->
-	</div><!-- .navigation -->
-	<?php } // End pagination IF Statement
-  } // End comment IF Statement
+		// Comment pagination.
+		if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) { ?>
+		<div class="navigation">
+			<div class="nav-previous fl"><?php previous_comments_link( __( '<span class="meta-nav">&larr;</span> Older Comments', infinity_text_domain ) ); ?></div>
+			<div class="nav-next fr"><?php next_comments_link( __( 'Newer Comments <span class="meta-nav">&rarr;</span>', infinity_text_domain ) ); ?></div>
+			<div class="fix"></div><!--/.fix-->
+		</div><!-- .navigation -->
+		<?php } // End pagination IF Statement
+	} // End comment IF Statement
 
-if ( ! empty($comments_by_type['pings']) ) { ?>
- 	<h3 id="comments-title"><?php  _e('Trackbacks/Pingbacks', infinity_text_domain); ?></h3>
- 	<ol class="commentlist">
-		<?php
-			/* Loop through and list the pings. Tell wp_list_comments()
-			 * to use list_pings() to format the pings.
-			 * If you want to overload this in a child theme then you can
-			 * define list_pings() and that will be used instead.
-			 * See list_pings() in /includes/theme-comments.php for more.
-			 */
-			wp_list_comments( array( 'callback' => 'list_pings', 'type' => 'pings' ) );
-		?>
-	</ol>
-<?php } // End pings IF Statement
- 
+	if ( ! empty($comments_by_type['pings']) ) { ?>
+		<h3 id="comments-title"><?php  _e('Trackbacks/Pingbacks', infinity_text_domain); ?></h3>
+		<ol class="commentlist">
+			<?php
+				/* Loop through and list the pings. Tell wp_list_comments()
+				 * to use list_pings() to format the pings.
+				 * If you want to overload this in a child theme then you can
+				 * define list_pings() and that will be used instead.
+				 * See list_pings() in /includes/theme-comments.php for more.
+				 */
+				wp_list_comments( array( 'callback' => 'list_pings', 'type' => 'pings' ) );
+			?>
+		</ol>
+	<?php } // End pings IF Statement
+
  } else {
 
  
