86ad1a54de1ce0eee94f362a773d6a6039953f95
r-a-y <r-a-y@gmx.com>
2018-07-16 12:17:56 -0700

    Refactor 'New Topic' button addition on group pages.
    - Replace deprecated create_function() call for PHP 7.2 with anonymous function
    - Add compatibility with BuddyPress 3.0
    - General code standards

--- engine/includes/buddypress-cbox.php
+++ engine/includes/buddypress-cbox.php
@@ -202,33 +202,28 @@ add_action( 'close_body', 'cbox_theme_buddypress_tour' );
  * @todo Remove this when bbPress addresses this.
  */
 function cbox_fix_bbp_new_topic_button() {
-	// if groups isn't active, stop now!
-	if ( ! bp_is_active( 'groups' ) )
+	// If groups component or bbPress isn't active, stop now!
+	if ( ! bp_is_active( 'groups' ) || ! function_exists( 'bbpress' ) ) {
 		return;
+	}
 
-	// if bbPress 2 isn't enabled, stop now!
-	if ( ! function_exists( 'bbpress' ) )
-		return;
-
-	// remove the 'New Topic' button
-	// this is done because the 'bp_get_group_new_topic_button' filter doesn't
-	// work properly
-	remove_action( 'bp_group_header_actions', 'bp_group_new_topic_button' );
-
-	// version of bp_is_group_forum() that works with bbPress 2
+	// Version of bp_is_group_forum() that works with bbPress 2.
 	$is_group_forum = bp_is_single_item() && bp_is_groups_component() && bp_is_current_action( 'forum' );
+	$is_group_topic = $is_group_forum && bp_is_action_variable( 'topic' );
 
-	// If these conditions are met, this button should not be displayed
-	if ( ! is_user_logged_in() || ! $is_group_forum || bp_is_group_forum_topic()|| bp_group_is_user_banned() )
-		return false;
+	// If these conditions are met, this button should not be displayed.
+	if ( ! is_user_logged_in() || ! $is_group_forum || $is_group_topic || bp_group_is_user_banned() ) {
+		return;
+	}
 
-	// create function to output new topic button
-	$new_button = create_function( '', "
-		// do not show in sidebar
-		if ( did_action( 'open_sidebar' ) )
+	// Add our customized 'New Topic' button.
+	add_action( 'bp_group_header_actions', function() {
+		// Do not show in sidebar.
+		if ( did_action( 'open_sidebar' ) ) {
 			return;
+		}
 
-		// render the button
+		// Render the button.
 		bp_button( array(
 			'id'                => 'new_topic',
 			'component'         => 'groups',
@@ -241,10 +236,7 @@ function cbox_fix_bbp_new_topic_button() {
 			'link_text'         => __( 'New Topic', 'buddypress' ),
 			'link_title'        => __( 'New Topic', 'buddypress' ),
 		) );
-	" );
-
-	// add our customized 'New Topic' button
-	add_action( 'bp_group_header_actions', $new_button );
+	} );
 
 }
 add_action( 'bp_actions', 'cbox_fix_bbp_new_topic_button' );
