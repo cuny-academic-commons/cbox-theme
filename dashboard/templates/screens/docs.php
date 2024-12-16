7d6168fdbe5c2ae27294771013c2f42d9bbbaea6
r-a-y <r-a-y@gmx.com>
2024-12-17 03:07:20 +0700

    Update CBOX Classic documentation link.
    This is called when clicking on the CBOX Theme admin dashboard's "User
    Docs" tab.

--- dashboard/templates/screens/docs.php
+++ dashboard/templates/screens/docs.php
@@ -1,1 +1,1 @@
-<iframe width="100%" height="800" src="http://commonsinabox.org/documentation" frameborder="0" allowfullscreen></iframe>
+<iframe width="100%" height="800" src="https://commonsinabox.org/cbox-classic-overview" frameborder="0" allowfullscreen></iframe>
