beb239a1a99cecaeae7d56a7956300d5c4bf59f6
r-a-y <r-a-y@gmx.com>
2025-07-29 08:00:24 -0700

    Load translations on 'init' as recommended by WordPress 6.7.

--- engine/infinity.php
+++ engine/infinity.php
@@ -182,7 +182,12 @@ require_once INFINITY_ICE_PATH . '/loader.php';
 ICE_Loader::init( INFINITY_ICE_URL );
 
 // setup translation
-load_theme_textdomain( INFINITY_TEXT_DOMAIN, WP_LANG_DIR . '/' . INFINITY_SLUG );
+add_action(
+	'init',
+	function() {
+		load_theme_textdomain( INFINITY_TEXT_DOMAIN, WP_LANG_DIR . '/' . INFINITY_SLUG );
+	}
+);
 
 // initialize enqueuer and configure actions
 if ( is_admin() ) {
