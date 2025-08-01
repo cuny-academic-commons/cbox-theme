80594cab737b39ba8bec4afda6aa238265b356c1
r-a-y <r-a-y@gmx.com>
2025-08-01 00:10:50 -0700

    Remove legacy forums code from group creation and admin pages.
    Legacy forums functionality (bbPress 1.x) was removed back in
    BuddyPress 3.0(!). If you have bbPress 2.x activated, this would cause
    a fatal error when navigating to the group create or admin pages.

--- groups/single/admin.php
+++ groups/single/admin.php
@@ -47,20 +47,6 @@
 
 	<?php do_action( 'bp_before_group_settings_admin' ); ?>
 
-	<?php if ( bp_is_active( 'forums' ) ) : ?>
-
-		<?php if ( bp_forums_is_installed_correctly() ) : ?>
-
-			<div class="checkbox">
-				<label><input type="checkbox" name="group-show-forum" id="group-show-forum" value="1"<?php bp_group_show_forum_setting(); ?> /> <?php _e( 'Enable discussion forum', 'buddypress' ); ?></label>
-			</div>
-
-			<hr />
-
-		<?php endif; ?>
-
-	<?php endif; ?>
-
 	<h4><?php _e( 'Privacy Options', 'buddypress' ); ?></h4>
 
 	<div class="radio">
